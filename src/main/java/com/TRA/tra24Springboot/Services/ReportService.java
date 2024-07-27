eacd27b6c6a98cb65edb805c475031c128b9a160
Mohammed27s <mohammed.23s@outlook.com>
2024-07-27 17:44:40 +0400

    The Jasper Report has been added to The Spring Boot project

--- src/main/java/com/TRA/tra24Springboot/Services/ReportService.java
+++ src/main/java/com/TRA/tra24Springboot/Services/ReportService.java
@@ -1,11 +1,10 @@
 package com.TRA.tra24Springboot.Services;
 
 
-import com.TRA.tra24Springboot.Models.Product;
-import com.TRA.tra24Springboot.Repositories.ProductRepository;
-import lombok.SneakyThrows;
+import com.TRA.tra24Springboot.DTO.ProductDTO;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
@@ -14,27 +13,30 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class ReportService {
 
-    private ProductRepository productRepository;
-    private String pathToReports;
+    @Autowired
+    private ProductServices productServices;
 
 
-    @SneakyThrows
-    public String generalReport() throws FileNotFoundException {
-        List<Product> productList = productRepository.findAll();
 
+    public void createProductReport() throws FileNotFoundException, JRException {
+        List<ProductDTO> productDTOList = productServices.getProducts();
+        UUID uuid = UUID.randomUUID();
 
-        File file = ResourceUtils.getFile("classpath:Product_report.jrxml");
+        String pathToSave = "C:\\Users\\TRA\\Desktop\\JasperReport";
+
+        File file = ResourceUtils.getFile("C:\\Users\\TRA\\Desktop\\MohammedInvoiceApplication\\src\\main\\resources\\Product_report.jrxml");
         JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(productList);
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("CreatesBy", "MyName");
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
-        JasperExportManager.exportReportToPdfFile(jasperPrint, pathToReports+"\\products.pdf");
-        return "Report generated : " + pathToReports+"\\products.pdf";
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(productDTOList);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,new HashMap<>(), dataSource);
+        String fileName = pathToSave + uuid+".pdf";
+        JasperExportManager.exportReportToPdfFile(jasperPrint, fileName);
+        System.out.print("Report is printed: "+ fileName);
+
 
     }
 
